e7d543913e76055fd3acf61c566f2c785d459148
Romina D-R <rdr2711@yahoo.com>
2018-06-20 20:13:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 let companyName = 'Scuber';
 
-if (companyName !== 'Scuber') {
-  return "AssertionError: expected [Function] to throw TypeErrorAssertionError@node_modules/chai/chai.js:9320:22assert@node_modules/chai/chai.js:239:31assertThrows@node_modules/chai/chai.js:2940:18methodWrapper@node_modules/chai/chai.js:7709:30[native code]test/indexTest.js:10:70"
-}
